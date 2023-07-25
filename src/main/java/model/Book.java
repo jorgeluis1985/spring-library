14206d3742a47a22cf370e56bfc811dbb46c8666
jorgeluis1985 <jorgeluiscamarcobaldovino@gmail.com>
2023-07-25 16:39:42 +0300

    Create model Book

---
+++ src/main/java/model/Book.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Book {
+    private int bookId;
+    private String name;
+    private String author;
+    private int year;
+
+    public Book() {
+    }
+
+    public Book(int bookId, String name, String author, int year) {
+        this.bookId = bookId;
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
